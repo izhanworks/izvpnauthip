d932a4d9555de189d98c6d81f0f5f2033f58cb69
Izhan Jafry <97201444+izhanworks@users.noreply.github.com>
2022-05-22 18:24:34 +0800

    Update scvpst-lite-ab.sh

--- scvpst-lite-ab.sh
+++ scvpst-lite-ab.sh
@@ -4001,9 +4001,9 @@ pub_menu=''
 ShellBot.InlineKeyboardButton --button 'pub_menu' --line 1 --text '• VMess •' --callback_data 'vmess'
 ShellBot.InlineKeyboardButton --button 'pub_menu' --line 1 --text '• VLess •' --callback_data 'vless'
 ShellBot.InlineKeyboardButton --button 'pub_menu' --line 2 --text '• Trojan •' --callback_data 'trojan'
-#ShellBot.InlineKeyboardButton --button 'pub_menu' --line 2 --text '• SSH •' --callback_data 'ssh'
 
-ShellBot.regHandleFunction --function ssh_publik --callback_data ssh
+
+
 ShellBot.regHandleFunction --function vmess_publik --callback_data vmess
 ShellBot.regHandleFunction --function vless_publik --callback_data vless
 ShellBot.regHandleFunction --function trojan_publik --callback_data trojan
